d2fb1ba9d23a18a04608978fb8840b067dae4c9e
jeremeyang <jeremeyang@ufl.edu>
2025-07-13 17:28:04 -0400

    chore: add note to reimplement buf
    buf uses a FIFO vector. popping from the front is a O(n) implementation, which will be O(n^2) in pipelining

--- server.cpp
+++ server.cpp
@@ -35,6 +35,7 @@ buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
 }
 
 // remove from the front
+// TODO: O(n) implementation
 static void buf_consume(std::vector<uint8_t> &buf, size_t n) {
     buf.erase(buf.begin(), buf.begin() + n);
 }
